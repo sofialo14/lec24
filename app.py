c4a236745c1a7ba486d6a5bffbd326c2af917140
Sofia Lotsoff <sofialo@umich.edu>
2018-04-10 10:02:43 -0400

    updates

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from flask import Flask, render_template
-import requests
-import json
+from flask import Flask, render_template, request
 import model
 
 app = Flask(__name__)
@@ -14,10 +12,27 @@ def index():
             <li><a href="/bball"> Men's Basketball </a></li>
         </ul>
     '''
-
-@app.route('/bball')
+@app.route('/bball', methods=['GET', 'POST'])
 def bball():
-    return render_template("seasons.html", seasons=model.get_bball_seasons())
+    if request.method == 'POST':
+        sortby = request.form['sortby']
+        sortorder = request.form['sortorder']
+        seasons = model.get_bball_seasons(sortby, sortorder)
+    else:
+        seasons = model.get_bball_seasons()
 
+    return render_template("seasons.html", seasons=seasons)
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+    else:
+        firstname = ''
+        lastname = ''
+    return render_template("hello.html", firstname=firstname, lastname=lastname)
 if __name__ == '__main__':
+    model.init_bball()
+    # print(model.get_bball_seasons())
     app.run(debug=True)
